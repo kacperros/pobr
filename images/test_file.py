fb354210e4c5c9175724c8c99f340a1f58b774e8
Kacper Roszczyna <kacper.ros94@gmail.com>
2017-01-04 21:28:31 +0100

    initial commit

---
+++ images/test_file.py
@@ -0,0 +1,3 @@
+import cv2
+
+img = cv2.imread("1.jpg")
---
+++ thresholders.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2
+
+
+def threshold_hue(image, lower_bound, upper_bound):
+    pass
+
+
+def threshold_value(lower_bound, upper_bound):
+    pass
