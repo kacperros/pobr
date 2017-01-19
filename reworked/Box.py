fixed distance calculation and cluster filtration

reworked/Box.py
@@ -38,16 +38,22 @@ class Box:
         return self.row_max - self.row_min
 
     def distance(self, box):
-        own_h = self.row_max-self.row_min
-        own_w = self.col_max - self.col_min
-        own_center = (int(self.row_max - own_h/2), int(self.col_max - own_w/2))
-        other_h = box.row_max-box.row_min
-        other_w = box.col_max - box.col_min
-        other_center = (box.row_max - other_h/2, box.col_max - other_w/2)
-        dr = np.abs(own_center[0] - other_center[0])
-        dc = np.abs(own_center[1] - other_center[1])
-        dist_r = dr - (own_h/2 + other_h/2)
-        dist_c = dc - (own_w/2 + other_w/2)
-        if dist_r < 0 and dist_c < 0:
+        vertical_dist = self.__get_vertical_dist(box)
+        horizontal_dist = self.__get_horizonta_dist(box)
+        return np.sqrt(vertical_dist ** 2 + horizontal_dist ** 2)
+
+    def __get_vertical_dist(self, box):
+        if box.row_min > self.row_max:
+            return box.row_min - self.row_max
+        elif box.row_max < self.row_min:
+            return self.row_min - box.row_max
+        else:
             return 0
-        return np.sqrt(dist_c ** 2 + dist_r ** 2)
+
+    def __get_horizonta_dist(self, box):
+        if box.col_max < self.col_min:
+            return self.col_min - box.col_max
+        elif box.col_min > self.col_max:
+            return box.col_min - self.col_max
+        else:
+            return 0